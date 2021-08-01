dbc0d06d37a4f3590c7862c8c88a2af63971caa0
Chixm <chixm2019@gmail.com>
2021-08-01 10:57:08 +0900

    add controller script

--- VRfight/Assets/MyScripts/GameRule.cs
+++ VRfight/Assets/MyScripts/GameRule.cs
@@ -7,17 +7,21 @@ public class GameRule : MonoBehaviour
 
     public GameObject player;
 
+    private GameObject camera;
+
     // Start is called before the first frame update
     void Start()
     {
         if (player != null) {
             Debug.Log("player has set for GameRule");
         }
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if(OVRInput.Get(OVRInput.Button.PrimaryHandTrigger)) {
             Debug.Log("Primary Trigger pressed");
         }
@@ -30,8 +34,33 @@ public class GameRule : MonoBehaviour
             Debug.Log("Two pressed");
         }
 
-        // 前の方向に移動する
+        // move to direction of thumb stick angled
         var forwarding = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        MoveForward(forwarding);
 
-    }    
+        // This code is for debug
+        if (Input.GetKeyDown(KeyCode.D)) {
+            MoveForward(new Vector2(1f,0f));
+        }
+        if (Input.GetKeyDown(KeyCode.A)) {
+            MoveForward(new Vector2(-1f,0f));
+        }
+        if (Input.GetKeyDown(KeyCode.W)) {
+            MoveForward(new Vector2(0f,1f));
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            MoveForward(new Vector2(0f,-1f));
+        }
+    }
+
+    Vector2 MoveForward(Vector2 controller) {
+        var currentRotate = camera.transform.rotation;
+        // move by force to the body.
+        var body = player.GetComponent<Rigidbody>();
+        // player's body is in x-z plane and controller axis is x-y, trade z-y.
+        body.AddForce(new Vector3(controller.x * 100, 0, controller.y * 100), ForceMode.Force);
+        return controller;
+    }
+
+
 }
